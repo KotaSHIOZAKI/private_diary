c4440140444cd61daac4533b2aeaf51d2491d455
KotaSHIOZAKI <ksw2070042@stu.o-hara.ac.jp>
2021-09-07 11:03:38 +0900

    フォーム画面の作成

--- diary/forms.py
+++ diary/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class InquiryForm(forms.form):
+class InquiryForm(forms.Form):
     name = forms.CharField(label='お名前', max_length=30)
     email = forms.EmailField(label='メールアドレス')
     title = forms.CharField(label='タイトル', max_length=30)
