2552bd5ec60dd5c096fba95f7330715068df47ec
KotaSHIOZAKI <ksw2070042@stu.o-hara.ac.jp>
2021-09-08 11:33:03 +0900

    メール送信処理の作成

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+#バックエンド定義
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- diary/views.py
+++ diary/views.py
@@ -1,9 +1,15 @@
 from diary.forms import InquiryForm
-from django.http.response import HttpResponse
+#from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 
 from .forms import InquiryForm
+
+import logging
+from django.urls import reverse_lazy
+
+#ロガーの取得
+logger = logging.getLogger(__name__)
 
 #インデックス（関数）
 def index(request):
@@ -20,3 +26,11 @@ class IndexView(generic.TemplateView):
 class InquiryView(generic.FormView):
     template_name = "diary/inquiry.html"
     form_class = InquiryForm
+    #「メッセージを送信しました。」画面のURL
+    success_url = reverse_lazy('diary:inquiry')
+
+    #メール送信
+    def form_valid(self, form):
+        form.send_email()
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
