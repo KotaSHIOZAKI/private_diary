83060ad8039587e6905e0f3012991cb654da164c
kotaSHIOZAKI <ksw2070042@stu.o-hara.ac.jp>
2021-09-08 16:04:24 +0900

    メッセージの表示処理

--- diary/views.py
+++ diary/views.py
@@ -2,9 +2,11 @@ from diary.forms import InquiryForm
 #from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views import generic
-
+#メッセージ送信用
 import logging
 from django.urls import reverse_lazy
+#メッセージ取得用
+from django.contrib import messages
 
 #ロガーの取得
 logger = logging.getLogger(__name__)
@@ -30,5 +32,6 @@ class InquiryView(generic.FormView):
     #メール送信
     def form_valid(self, form):
         form.send_email()
+        messages.success(self.request, 'メッセージを送信しました。')
         logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
