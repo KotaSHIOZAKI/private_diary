b7c363801f872a8312d67a0cd74d8e0e595d4613
KotaSHIOZAKI <ksw2070042@stu.o-hara.ac.jp>
2021-09-10 12:24:33 +0900

    日記一覧表示機能の作成

--- diary/urls.py
+++ diary/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     
     #問い合わせ
     path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
+
+    #日記一覧表示
+    path('diary-list/', views.DiaryListView.as_view(), name="diary_list"),
 ]
--- diary/views.py
+++ diary/views.py
@@ -7,6 +7,9 @@ import logging
 from django.urls import reverse_lazy
 #メッセージ取得用
 from django.contrib import messages
+#日記一覧用
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Diary
 
 #ロガーの取得
 logger = logging.getLogger(__name__)
@@ -35,3 +38,11 @@ class InquiryView(generic.FormView):
         messages.success(self.request, 'メッセージを送信しました。')
         logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
+
+class DiaryListView(LoginRequiredMixin, generic.ListView):
+    model = Diary
+    template_name = 'diary_list.html'
+
+    def get_queryset(self):
+        diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
+        return diaries
