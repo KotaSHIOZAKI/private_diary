730762d0b65c96aeb0df58b27cbf6f9e5a6d0bc0
kotaSHIOZAKI <ksw2070042@stu.o-hara.ac.jp>
2021-09-13 14:46:52 +0900

    日記詳細機能の実装

--- diary/views.py
+++ diary/views.py
@@ -48,3 +48,7 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at') #マイナス＝昇順
         return diaries
+
+class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Diary
+    template_name = 'diary/diary_detail.html'
